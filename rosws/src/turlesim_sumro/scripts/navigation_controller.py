2dc3e6e3296beadd3bf55de1714320861854f4b3
Luis Diego Garcia Castro <luis.d.garciacastro@campus.tu-berlin.de>
2019-08-11 21:40:22 +0200

    Refactor navigator methods and cube properties

--- rosws/src/turlesim_sumro/scripts/navigation_controller.py
+++ rosws/src/turlesim_sumro/scripts/navigation_controller.py
@@ -20,29 +20,45 @@ References:
 
 import rospy
 import actionlib
+from actionlib_msgs.msg import GoalStatus
 from geometry_msgs.msg import Pose, Point, Quaternion, Twist
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 import tf
 
 class Cube(object):
-    """
-    Represents a cube in the map
-    """
+    """ Represents a cube in the map """
     
     def __init__(self, number):
+        self._visited = False
         self._number = number
+        self._pose = None
     
     @property
-    def frame_id(self):
-        return 'Cube{}'.format(self._number)
+    def number(self):
+        return self._number
     
     @property
     def is_good(self):
         # rule: odd numbers are good cubes
-        return self._number % 2 != 0
+        return self.number % 2 != 0
+    
+    @property
+    def visited(self):
+        return self._visited
+
+    @property
+    def pose(self):
+        return self._pose
+    
+    @pose.setter
+    def set_pose(self, pose):
+        self._pose = pose
+    
+    def visit(self):
+        self._visited = True
 
     def __str__(self):
-        return 'Cube #{}'.format(self._number)
+        return 'Cube (number={}, visited={})'.format(self.number, self.visited)
 
 class NavigationController(object):
     """
@@ -50,54 +66,72 @@ class NavigationController(object):
     visit each good cube and avoids the bad cubes.
     """
 
-    def __init__(self, cubes=[]):
+    def __init__(self):
         rospy.init_node('navigation_controller', anonymous=False)        
         rospy.on_shutdown(self._shutdown)
 
-        self._cubes = cubes
+        # We know there will be 7 cubes in total for the task
+        self._cubes = [Cube(i) for i in range(1, 8)]
+
         self._transform_listener = tf.TransformListener()
         self._move_base_client = actionlib.SimpleActionClient('move_base', MoveBaseAction)
         self._cmd_vel_pub = rospy.Publisher('base_footprint/cmd_vel', Twist, queue_size=5)
 
     def get_cube_pose(self, cube):
-        try:
-            self._transform_listener.waitForTransform('world', cube.frame_id, rospy.Time(), rospy.Duration(4.0))
-            (position, orientation) = self._transform_listener.lookupTransform('world', cube.frame_id, rospy.Time(0))
-            return position, orientation
-        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
-            rospy.logerr('Error getting position and orientation: {}'.format(e))
-        return None, None
+        """
+        Returns the transform from the cube's coordinate frame
+        to the world's coordinate frame
+        """
+        return self._get_frame_transform('world', 'Cube{}'.format(cube.number))
 
     def move_to_cube(self, cube):
-        """ Creates a goal to move towards a cube
-            and sends that goal to the move base server
         """
-        if cube.is_good:
-            rospy.loginfo('Moving towards good cube: {}'.format(cube))
+        Creates a goal to move towards a cube
+        and sends that goal to the move base server
+        """
+        if cube.is_good and not cube.visited:
+            rospy.loginfo('Attempting to move towards {}'.format(cube))
             self._move_base_client.wait_for_server(rospy.Duration(4.0))
+
             goal = MoveBaseGoal()
             goal.target_pose.header.frame_id = 'world'
             goal.target_pose.header.stamp = rospy.Time.now()
-            position, orientation = self.get_cube_pose(cube)
-            goal.target_pose.pose = Pose(Point(*position), Quaternion(*orientation))
-            self._move(goal)
+            pose = self.get_cube_pose(cube)
+            goal.target_pose.pose = pose        
+            if self._move(goal):
+                cube.visit()
+            else:
+                rospy.loginfo('Failed to move towards {}'.format(cube))
         else:
-            rospy.loginfo('Skipping bad cube: {}'.format(cube))
+            rospy.loginfo('Skipping {}'.format(cube))
 
     def run(self):
-        i = 0
-        while not rospy.is_shutdown() and i < len(self._cubes):
-            self.move_to_cube(self._cubes[i])
-            i += 1
+        pending_cubes = self._pending_cubes()
+        while not rospy.is_shutdown() and pending_cubes:
+            self.move_to_cube(pending_cubes[0])
+            pending_cubes = self._pending_cubes()
+
+    def _pending_cubes(self):
+        """ 
+        Returns a list of the discovered (good) cubes
+        TODO: Sort the list by distance to the robot
+        """
+        is_pending = lambda cube: cube.is_good and not cube.visited
+        return filter(is_pending, self._cubes)
 
     def _move(self, goal):
         """ 
         Adapted from source 1)
+        Returns True is the move was successful
         """
+        # Did we reach our goal?
+        success = False
+
         # Send the goal pose to the MoveBaseAction server
         self._move_base_client.send_goal(goal)
         
         # Allow 1 minute to get there
+        # TODO: adjust duration for a real-case scenario
         finished_within_time = self._move_base_client.wait_for_result(rospy.Duration(60)) 
         
         # If we don't get there in time, abort the goal
@@ -107,12 +141,30 @@ class NavigationController(object):
         else:
             # We made it!
             state = self._move_base_client.get_state()
+            success = state == GoalStatus.SUCCEEDED
             rospy.loginfo('Goal state: {}'.format(state))
+        return success
+
+    def _get_frame_transform(self, target_frame, source_frame, max_retries=None):
+        """
+        Returns the transform from source_frames' coordinate frame
+        to the target_frame's coordinate frame
+        """
+        pose = None
+        current_retries = 0
+        while pose is None and (max_retries is None or current_retries < max_retries):
+            try:
+                self._transform_listener.waitForTransform(target_frame, source_frame, rospy.Time(), rospy.Duration(4.0))
+                position, orientation = self._transform_listener.lookupTransform(target_frame, source_frame, rospy.Time(0))
+                pose = Pose(Point(*position), Quaternion(*orientation))
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
+                rospy.logerr('Error getting frame transform ({} -> {}): {}'.format(source_frame, target_frame, e))
+                current_retries += 1
+                pose = None
+        return pose
 
     def _shutdown(self):
-        """
-        Taken from source 1)
-        """
+        """ Taken from source 1) """
         rospy.loginfo('Stopping the robot...')
         # Cancel any active goals
         self._move_base_client.cancel_goal()
@@ -122,10 +174,5 @@ class NavigationController(object):
         rospy.sleep(1)
 
 if __name__ == '__main__':
-    
-    # Initialize 7 cubes
-    cubes = [Cube(i) for i in range(1, 8)]
-
-    # Initialize controller
-    controller = NavigationController(cubes)
+    controller = NavigationController()
     controller.run()
