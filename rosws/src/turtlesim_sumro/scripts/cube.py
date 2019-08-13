7be27e0eabae009aed54c853eadeeffabf2779be
Luis Diego Garcia <luis.d.garciacastro@campus.tu-berlin.de>
2019-08-13 19:25:08 +0200

    Refactor controller - Fixes #2 - navigation_controller is now mission_controller

--- rosws/src/turtlesim_sumro/scripts/cube.py
+++ rosws/src/turtlesim_sumro/scripts/cube.py
@@ -1,8 +1,9 @@
+from geometry_msgs.msg import Point32
+
 class Cube(object):
     """ Represents a cube in the map """
     
     def __init__(self, number):
-        self._visited = False
         self._number = number
         self._xpos = None
         self._ypos = None
@@ -10,26 +11,6 @@ class Cube(object):
     @property
     def number(self):
         return self._number
-
-    @property
-    def visit_pending(self):
-        """ Returns True if a visit to this cube is pending """
-        return self.is_target and not self.visited
-    
-    @property
-    def is_target(self):
-        """ Returns True if this cube should be visited """
-        return self.number % 2 != 0
-    
-    @property
-    def visited(self):
-        """ Returns True if this cube is marked as visited """
-        return self._visited
-
-    @property
-    def frame_id(self):
-        """ Returns a string representing this cube's frame id """
-        return 'Cube{0}'.format(self.number)
 
     @property
     def xpos(self):
@@ -40,16 +21,60 @@ class Cube(object):
         return self._ypos
     
     @xpos.setter
-    def set_xpos(self, xpos):
+    def xpos(self, xpos):
         self._xpos = xpos
     
     @ypos.setter
-    def set_ypos(self, ypos):
+    def ypos(self, ypos):
         self._ypos = ypos
-    
-    def visit(self):
-        """ Marks cube as visited """
-        self._visited = True
 
+    @property
+    def bounding_box(self):
+        """
+        * = offset
+
+        v1 = (x-offset, y+offset)
+        v2 = (x+offset, y+offset)
+        v3 = (x+offset, y-offset)
+        v4 = (x-offset, y-offset)
+
+        ^   Bounding box:
+        |   v1----------v2
+        |   |           |
+        |   |   (x,y) * *
+        |   |    *      |
+        |   v4---*------v3
+        |------------------------>x
+        |
+        |
+        y
+        
+        """
+
+        # We asumme each cube is 20 cm
+        offset = 0.002
+        # If the cube is not a target we
+        # should "inflate" it's size
+        # so that it includes it's forbidden zone (50 cm)
+        if not self.is_target:
+            offset += 0.005
+
+        v1 = Point32(x=self.xpos-offset, y=self.ypos+offset)
+        v2 = Point32(x=self.xpost+offset, y=self.ypos+offset)
+        v3 = Point32(x=self.xpos+offset, y=self.ypos-offset)
+        v4 = Point32(x=self.xpos-offset, y=self.ypos-offset)
+        
+        return [v1, v2, v3, v4]
+    
+    @property
+    def frame_id(self):
+        """ Returns a string representing this cube's frame id """
+        return 'C{:03d}'.format(self.number)
+    
+    @property
+    def is_target(self):
+        """ Returns True if this cube should be visited """
+        return self.number % 2 != 0
+    
     def __str__(self):
-        return 'Cube (number={}, visited={})'.format(self.number, self.visited)
+        return 'Cube (number={}, xpos={}, ypos={}, frame={})'.format(self.number, self.xpos, self.ypos, self.frame_id)
---
+++ rosws/src/turtlesim_sumro/scripts/run_mission.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from mission_controller import MissionController
+
+if __name__ == '__main__':
+    try:
+        rospy.init_node('mission_runner', anonymous=False)        
+        controller = MissionController(n_total_cubes=7, n_target_cubes=3)
+        controller.run()
+    except rospy.ROSInitException:
+        rospy.loginfo('Simulation mission ended')
