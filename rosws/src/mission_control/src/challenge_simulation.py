daf0131080bd18b70aa33ee18cf1bd2ffb40dcab
Adolfo E. García Castro <adolfo.e.garciacastro@campus.tu-berlin.de>
2019-08-14 14:23:22 +0200

    Add simulation challenge files

---
+++ rosws/src/mission_control/src/challenge_simulation.py
@@ -0,0 +1,162 @@
+#!/usr/bin/env python
+
+"""
+A node that simulates our rover's challenge.
+
+Motivation: have different scenarios to test our code.
+
+
+References:
+
+1) https://wiki.ros.org/turtlesim
+
+2) http://wiki.ros.org/ROS/Tutorials/WritingServiceClient%28python%29
+
+3) https://github.com/ROS4SPACE/aruco_analyzer/blob/e50ab9183b8903ba5699115788f7f813f713aa6f/aruco_analyzer/src/aruco_analyzer/ros_wrapper/tf_broadcaster.py
+
+4) http://docs.ros.org/melodic/api/turtle_tf/html/turtle__tf__broadcaster_8py_source.html
+"""
+
+import math
+import random
+import rospy
+import tf2_ros
+import turtlesim
+
+from cube import Cube
+from geometry_msgs.msg import TransformStamped, Quaternion
+from tf.transformations import quaternion_from_euler
+from turtlesim.srv import *
+import turtlesim.msg
+
+class ChallengeSimulation(object):
+
+    min_x = 1.5
+    max_x = 9.0
+
+    min_y = 1.5
+    max_y = 9.0
+
+    influence_area_radius = 5.5
+
+    # Use a different orientation for evil and good cubes to tell them apart visually.
+    evil_cube_orientation = math.radians(90)
+    good_cube_orientation = math.radians(-90)
+
+    def __init__(self):
+        # We know there will be 7 cubes in total for the task
+        self._cubes = [Cube(i) for i in range(1, 8)]
+
+        self._tf_buffer = tf2_ros.Buffer()
+        self._tf_listener = tf2_ros.TransformListener(self._tf_buffer)
+        self._tf_broadcaster = tf2_ros.TransformBroadcaster()
+        self._base_footprint_pos_subscriber = None
+        self._timer = None
+        self._global_frame_id = ''
+        
+    def _initialize_coordinates(self):
+        # Create coordinates set and add first point
+        coordinates = []
+        coordinates.append((self._uniform_dist(self.min_x, self.max_x), self._uniform_dist(self.min_y, self.max_y)))
+        # Create the remaining points
+        N = len(self._cubes)
+        while(len(coordinates) != N):
+            point = None
+            while point is None:
+                # our new candidate point
+                x = self._uniform_dist(self.min_x, self.max_x)
+                y = self._uniform_dist(self.min_y, self.max_y)
+                for (x2, y2) in coordinates:
+                    if self._is_inside_cube_area(x,y,x2,y2):
+                        rospy.loginfo('Collision between turtles detected!')
+                        break
+                point = (x,y)
+                coordinates.append(point)
+
+        assert(len(coordinates) == 7)
+        
+        for cube, point in zip(self._cubes, coordinates):
+            cube.xpos = point[0]
+            cube.ypos = point[1]
+
+    def _is_inside_cube_area(self, x1, y1, x2, y2):
+        ''' Returns True if cube2's position is inside cube1's area of influece '''
+        xcheck = x1 - self.influence_area_radius <= x2 and x2 <= x1 + self.influence_area_radius
+        ycheck = y1 - self.influence_area_radius <= y2 and y2 <= y1 + self.influence_area_radius
+        return xcheck and ycheck
+
+    def _uniform_dist(self, a=0.0, b=1.0):
+        sample = a + (b-a)*random.random()
+        assert(sample < b)
+        assert(sample >= a)
+        return sample
+
+    def _create_turtles(self):
+        rospy.wait_for_service('spawn')
+        for cube in self._cubes:
+            try:
+                spawn = rospy.ServiceProxy('spawn', Spawn)
+                yaw_orientation = math.radians(90 if cube.is_target else -90)
+                cube.orientation = Quaternion(*quaternion_from_euler(0.0, 0.0, yaw_orientation))
+                response = spawn(cube.xpos, cube.ypos, yaw_orientation, cube.frame_id)
+            except rospy.ServiceException, e:
+                rospy.loginfo('Failed to spawn turtle. ' + str(e))
+
+    def _create_rover(self):
+        rospy.wait_for_service('spawn')
+        try:
+            spawn = rospy.ServiceProxy('spawn', Spawn)
+            yaw_angle = math.radians(self._uniform_dist(0.0, 360.0))
+            response = spawn(0.5, 0.5, yaw_angle, 'base_footprint')
+        except rospy.ServiceException, e:
+            rospy.loginfo('Failed to spawn turtle. ' + str(e))
+        else:
+            self._base_footprint_pos_subscriber = rospy.Subscriber('/base_footprint/pose', turtlesim.msg.Pose, callback=self._broadcast_rover_tf_callback)
+
+    def _broadcast_rover_tf_callback(self, msg):
+        t = TransformStamped()
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = self._global_frame_id
+        t.child_frame_id = 'base_footprint'
+
+        t.transform.translation.x = msg.x
+        t.transform.translation.y = msg.y
+        t.transform.translation.z = 0.0
+        t.transform.rotation = Quaternion(*quaternion_from_euler(0.0, 0.0, msg.theta))
+  
+        self._tf_broadcaster.sendTransform(t)
+
+    def _broadcast_tfs_callback(self, event):
+        for cube in self._cubes:
+            t = TransformStamped()
+            t.header.stamp = rospy.Time.now()
+            t.header.frame_id = self._global_frame_id
+            t.child_frame_id = cube.frame_id
+
+            t.transform.translation.x = cube.xpos
+            t.transform.translation.y = cube.ypos
+            t.transform.translation.z = 0.0
+            t.transform.rotation = cube.orientation
+            
+            self._tf_broadcaster.sendTransform(t)
+
+    def run(self):
+        self._global_frame_id = rospy.get_param('global_frame_id', default='world')
+        self._initialize_coordinates()
+        self._create_turtles()
+        self._create_rover()
+        self._timer = rospy.Timer(rospy.Duration(1.0/2.0), self._broadcast_tfs_callback) # 2 hz
+
+def main():
+    try:
+        random.seed()
+        rospy.init_node('sumro_challenge_simulation', anonymous=False)
+        challenge_sim = ChallengeSimulation()
+        challenge_sim.run()
+        rospy.spin()
+    except rospy.ROSInitException:
+        rospy.loginfo('Could not start challenge simulation.')
+
+
+if __name__ == '__main__':
+    main()
