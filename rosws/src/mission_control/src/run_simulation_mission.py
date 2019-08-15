82793073311630458dadba9d6cbc829bbc572e55
Adolfo E. García Castro <adolfo.e.garciacastro@campus.tu-berlin.de>
2019-08-15 04:27:27 +0200

    Polish runners

--- rosws/src/mission_control/src/run_simulation_mission.py
+++ rosws/src/mission_control/src/run_simulation_mission.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python
 import rospy
 
-#from mission_controller import MissionController
 from mission_sm import create_mission_state_machine
 
 if __name__ == '__main__':
     try:
         rospy.init_node('simulation_mission_runner', anonymous=False)        
-        #controller = MissionController(n_total_cubes=7, n_target_cubes=3)
-        #controller.yaw_rotation_scan()
-        #controller.run()
         sm = create_mission_state_machine(n_total_cubes=7, n_target_cubes=4)
         outcome = sm.execute()
     except rospy.ROSInitException:
